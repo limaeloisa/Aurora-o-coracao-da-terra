d0eefe283f72feb11e8c3b86ab9ac0aa4d5f0f97
davisonerik <145374995+davisonerik@users.noreply.github.com>
2025-11-17 16:37:01 -0300

    puzzle1.1

--- jogo aurora/Assets/scripts puzzle/GameManager.cs
+++ jogo aurora/Assets/scripts puzzle/GameManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -11,6 +12,20 @@ public class GameManager : MonoBehaviour
     [Header("Game Objects")]
     [SerializeField] private Tile tilePrefab;
     [SerializeField] private Transform gameArea;
+
+    [Header("Audio Setup")] [SerializeField]
+    private float duration = 0.2f;
+    [SerializeField] private AudioSource audioSource;
+
+    enum GameMode
+    {
+        None,
+        Menu,
+        Listeng,
+        Playing
+    }
+    
+    private GameMode gameMode = GameMode.None;
 
      void Start()
      {
@@ -41,5 +56,47 @@ public class GameManager : MonoBehaviour
         float scale = 6f / numRows;
         gameArea.localScale = Vector3.one * scale;
 
+        gameMode = GameMode.Menu;
+        StartCoroutine(MenuTileAnimation());
+
+
      }
+
+     private IEnumerator MenuTileAnimation()
+     {
+         while (gameMode == GameMode.Menu)
+         {
+             yield return FlashTiles(Random.Range(0, numTiles));
+             yield return new WaitForSeconds(duration);
+         }
+        
+     }
+     
+     private IEnumerator FlashTiles(int index)
+     {
+         tile[index].TurnOn();
+         yield return new WaitForSeconds(duration);
+         tile[index].TurnOff();
+         
+     }
+
+     public void PlayLightAndTone(int index)
+     {
+         StartCoroutine(FlashTiles(index));
+         PlayTone(index);
+     }
+
+     private void PlayTone(int index)
+     {
+         if (numTiles > 1)
+         {
+             audioSource.pitch = Mathf.Lerp(0.5f, 2.0f, index / (numTiles - 1f) );
+         }
+         
+         double currenTime = AudioSettings.dspTime;
+         audioSource.PlayScheduled(currenTime);
+         audioSource.SetScheduledEndTime(currenTime + duration);
+        
+     }
+
 }
--- jogo aurora/Assets/scripts puzzle/Tile.cs
+++ jogo aurora/Assets/scripts puzzle/Tile.cs
@@ -29,7 +29,7 @@ public class Tile : MonoBehaviour
 
     private void OnMouseDown()
     {
-        
+        gameManager.PlayLightAndTone(tileId);
     }
  
 }
